a08bec744a4a5c039ad6694f9fa4f5049a35cb4c
quatbatieu <102890801+quatbatieu@users.noreply.github.com>
2024-11-06 17:01:33 +0700

    fix page saladin (#45)

--- src/helper/CheckApiKey.js
+++ src/helper/CheckApiKey.js
@@ -1,14 +1,16 @@
 export const CheckApiKey = () => {
-  let params = new URLSearchParams(document.location.search);
+  let params = new URLSearchParams(document.location.search)
+  const searchParams = window.location.href
+  const webSaladin = 'saladin.ttdk.com.vn'
+
+  if (searchParams.includes(webSaladin)) {
+    return window.location.href = "https://saladin.ttdk.com.vn/booking?apikey=fe58f4e7-29ac-4ade-86b1-d51a3b0602a5"
+  }
   let apikey
-  const _TTDKMiniAppKey=process.env.REACT_APP_BOOKING_API_KEY
-  if(_TTDKMiniAppKey){
-    return(
-      apikey = _TTDKMiniAppKey
-    )
-  }else{
-    return(
-      apikey=params.get("apikey")
-    )
+  const _TTDKMiniAppKey = process.env.REACT_APP_BOOKING_API_KEY
+  if (_TTDKMiniAppKey) {
+    return (apikey = _TTDKMiniAppKey)
+  } else {
+    return (apikey = params.get('apikey'))
   }
 }
