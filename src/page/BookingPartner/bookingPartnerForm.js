30d78168f59898b79c34264b021f0ae37d3b26a4
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2025-01-10 12:01:21 +0700

    Revert "Merge pull request #63 from FreeDevTeam/vanSo/LoadStation" (#64)
    This reverts commit e267970c0b418b65ae52e89e73acc782be9d6691, reversing
    changes made to 7ff36cfd0bc17cb1fef27be161b532c1aef1e7ea.

--- src/page/BookingPartner/bookingPartnerForm.js
+++ src/page/BookingPartner/bookingPartnerForm.js
@@ -768,15 +768,8 @@ function BookingPartnerForm({form, setTabKey, zaloUserName,zaloUserPhone}) {
   function getStations(filter = null, callback = null) {
     setSelectedBookingStation(false)
     filter = filter ? filter : customerParam
-    const newFilter = {
-      ...filter,
-      filter:{
-        ...filter?.filter,
-        scheduleType: dataBookingParam?.scheduleType
-      }
-    }
     setIsVisible((prev) => ({ ...prev, stationsId: true }))
-    BookingService.getStationList(newFilter)
+    BookingService.getStationList(filter)
       .then((data) => {
         setIsVisible((prev) => ({ ...prev, stationsId: false }))
         let tmp = data?.data || []
