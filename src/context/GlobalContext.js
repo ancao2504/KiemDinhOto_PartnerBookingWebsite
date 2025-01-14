6a65409d7377cbd21962ed8a12d1ad221c78f947
caonguyen <caonguyencmg2010@gmail.com>
2025-01-14 18:12:07 +0700

    xoá test

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -37,8 +37,8 @@ const WarningNotify = ({ isModalOpen, onClose, onConfirm }) => {
 
 export const GlobalProvider = ({ children }) => {
     const [globalState, setGlobalState] = useState({
-        phoneNumber: "0352625458",
-        userName: "test",
+        phoneNumber: "",
+        userName: "",
         isZaloApp: process.env.REACT_APP_ZALO_AUTH_ENABLE
     });
 
