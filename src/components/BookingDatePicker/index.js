82c11c1fec18223ff3cb94cdf8048f5fcc7906c3
sang901-dev <216698415+sang901-dev@users.noreply.github.com>
2026-04-08 23:38:44 +0700

    Revert "Squashed commit of the following:"
    This reverts commit a578d75ee6484507bf1cfb17e6d34db998abecf9.

--- src/components/BookingDatePicker/index.js
+++ src/components/BookingDatePicker/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import moment from 'moment'
 import { LeftOutlined, RightOutlined } from '@ant-design/icons'
 import { Button, Empty } from 'antd'
@@ -8,26 +8,42 @@ import { DATE_DISPLAY_FORMAT } from '../../constants/dateFormats'
 import LoadingPopup from '../LoadingPopup'
 
 moment.updateLocale('vi', {
-  weekdays: ['CN', 'Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7']
+  weekdays: ['CN', 'Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7'] // Cấu hình ngày trong tuần
 })
 
-const MONTH_YEAR_FORMAT = 'MM/YYYY'
+const MONTH_YEAR_FORMAT = 'MM/YYYY' // Định nghĩa định dạng tháng/năm
+
+function getDaysInMonthFromToday() {
+  const currentDate = moment();
+  const firstDayOfNextMonth = currentDate.clone().endOf('month').add(1, 'day');
+  const daysInMonth = [];
+  let currentDay = currentDate.clone();
+
+  while (currentDay.isBefore(firstDayOfNextMonth, 'day')) {
+    daysInMonth.push({scheduleDate:currentDay.clone()});
+    currentDay = currentDay.add(1, 'day');
+  }
+
+  return daysInMonth;
+}
+
 
 export default function BookingDatePicker({
   selectedDate = '',
   disabled = true,
   setSelectedDate,
   listBookingDate = [],
+  bookingConfig =[],
   currentMonth,
   setCurrentMonth,
-  loading = false,
+  loading= false,
   minAvailableMonth = moment().format(DATE_DISPLAY_FORMAT)
 }) {
   const disablePickDate = disabled || !listBookingDate?.length
-
   const handlePickDate = (date) => {
-    if (disabled) return
-    if (isDisabledDate(date)) return
+    
+    if(disabled) return
+    if(isDisabledDate(date)) return
     setSelectedDate(date?.scheduleDate)
   }
 
@@ -35,33 +51,41 @@ export default function BookingDatePicker({
     const checkCurrentMonth = moment(currentMonth, DATE_DISPLAY_FORMAT).subtract(1, 'months')
     if (checkCurrentMonth.isSameOrBefore(moment(), 'month')) {
       setCurrentMonth(moment())
-    } else {
-      setCurrentMonth(moment(currentMonth, DATE_DISPLAY_FORMAT).subtract(1, 'months'))
-    }
-    if (setSelectedDate) setSelectedDate(undefined)
+    } else setCurrentMonth(moment(currentMonth, DATE_DISPLAY_FORMAT).subtract(1, 'months'))
   }
 
   const increaseMonth = () => {
     setCurrentMonth(moment(currentMonth, DATE_DISPLAY_FORMAT).startOf('month').add(1, 'months'))
-    if (setSelectedDate) setSelectedDate(undefined)
   }
 
   const getDisplayTextByScheduleDateStatus = (element) => {
-    if (!element?.displayText) {
-      return ''
+    const fullSchedule = isDisabledDate(element);
+    const enableBookingHandler = (bookingConfig || []).some((item) => item?.enableBooking);
+    if (element.scheduleDateStatus === 0) {
+        if (fullSchedule) {
+            return <div style={{ color: 'var(--error-btn-color)' }}>Đã đầy</div>;
+        } else {
+            if (element?.totalBookingSchedule) {
+                return enableBookingHandler ? `${element?.totalBookingSchedule}` : `Đang chờ ${element?.totalBookingSchedule}`;
+            } else {
+                return enableBookingHandler ? '' : 'Đang chờ 0';
+            }
+        }
+    } else {
+        return element?.totalBookingSchedule || element?.totalSchedule ? `${element?.totalBookingSchedule || 0}/${element?.totalSchedule}` : '';
     }
+};
 
-    if (element?.isFull) {
-      return <div style={{ color: 'var(--error-btn-color)' }}>{element.displayText}</div>
-    }
+const isDisabledDate = (element)=>{
+  let fullSchedule = false;
 
-    return element.displayText
+  if (element?.totalSchedule > 0) {
+      fullSchedule = element?.totalBookingSchedule >= element?.totalSchedule;
   }
+  return fullSchedule
+}
 
-  const isDisabledDate = (element) => {
-    return !!element?.disabled
-  }
-
+const listDateData = listBookingDate
   return (
     <div className={`booking-date-picker`}>
       <div className={`booking-date-picker_header`}>
@@ -69,7 +93,7 @@ export default function BookingDatePicker({
         <div>
           <Button
             icon={<LeftOutlined className="booking-date-picker__day--text" />}
-            disabled={disabled ? true : moment(currentMonth, DATE_DISPLAY_FORMAT).isSameOrBefore(moment(minAvailableMonth, DATE_DISPLAY_FORMAT), 'month')}
+            disabled={disabled ? true : moment(currentMonth, DATE_DISPLAY_FORMAT).isSameOrBefore(moment(minAvailableMonth,DATE_DISPLAY_FORMAT), 'month')}
             onClick={decreaseMonth}
           />
           <span className="booking-date-picker__day--text ms-2 me-2">
@@ -78,34 +102,23 @@ export default function BookingDatePicker({
           <Button disabled={disabled} icon={<RightOutlined className="booking-date-picker__day--text" />} onClick={increaseMonth} />
         </div>
       </div>
-      {!loading ? (
-        <div className="booking-date-picker_content">
-          {listBookingDate?.length ? (
-            listBookingDate.map((value, index) => {
-              const dateObj = moment(value?.scheduleDate, DATE_DISPLAY_FORMAT).locale('vi')
-              const isDateDisabled = isDisabledDate(value)
-
-              return (
-                <div
-                  index={index}
-                  style={{ cursor: disablePickDate || isDateDisabled ? 'not-allowed' : 'pointer' }}
-                  onClick={() => handlePickDate(value)}
-                  className={`${isDateDisabled ? 'disabled-date' : ''} booking-date-picker__day ${selectedDate === value?.scheduleDate ? 'active' : ''}`}
-                  key={index}>
-                  <div className="booking-date-picker__day--text">{dateObj.format('dddd')}</div>
-                  <div className="booking-date-picker__day--text">{dateObj.format('DD')}</div>
-                  <span className="wrap-text">{getDisplayTextByScheduleDateStatus(value)}</span>
-                </div>
-              )
-            })
-          ) : (
-            <Empty description={'Không có ngày hẹn trống'}></Empty>
-          )}
-        </div>
-      ) : (
-        <LoadingPopup type="content" />
-      )}
+      {!loading ? <div className="booking-date-picker_content">
+        {listDateData?.length ? listDateData.map((value, index) => {
+          const dateObj = moment(value?.scheduleDate, DATE_DISPLAY_FORMAT).locale('vi')
+          return (
+            <div
+              index={index}
+              style={{ cursor: disablePickDate ? "not-allowed" : "pointer", height: disablePickDate ? 70 : 100 }}
+              onClick={() => handlePickDate(value)}
+              className={`${isDisabledDate(value) ? 'disabled-date' : ''} booking-date-picker__day ${selectedDate === value?.scheduleDate ? 'active' : ''}`}
+              key={index}>
+              <div className="booking-date-picker__day--text">{dateObj.format('dddd')}</div>
+              <div className="booking-date-picker__day--text">{dateObj.format('DD')}</div>
+              <span className="wrap-text">{getDisplayTextByScheduleDateStatus(value)}</span>
+            </div>
+          )
+        }): <Empty description={"Không có ngày hẹn trống"}></Empty>}
+      </div> : <LoadingPopup type="content"/>}
     </div>
   )
 }
-
